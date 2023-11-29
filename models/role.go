212dcfd825e9da5f80ab561ee035d7ba8cf9811a
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2023-11-29 19:49:38 +0100

    Add helper methods for role

--- models/role.go
+++ models/role.go
@@ -1,13 +1,18 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
 
 const (
-	CREATE_EVENT uint8 = 1
-	MODIFY_EVENT uint8 = 2
-	DELETE_EVENT uint8 = 4
-	MODERATE     uint8 = 8
-	ADMIN        uint8 = 16
+	USER         uint8 = 1
+	CREATE_EVENT uint8 = 2
+	MODIFY_EVENT uint8 = 4
+	DELETE_EVENT uint8 = 8
+	MODERATE     uint8 = 16
+	ADMIN        uint8 = 32
 )
 
 type Role struct {
@@ -15,4 +20,40 @@ type Role struct {
 	Name        string
 	Permissions uint8
 	Default     bool
+}
+
+func (r *Role) AddPermission(perm uint8) {
+	if !r.HasPermission(perm) {
+		r.Permissions += perm
+	}
+}
+
+func (r *Role) RemovePermission(perm uint8) {
+	if r.HasPermission(perm) {
+		r.Permissions -= perm
+	}
+}
+
+func (r *Role) ResetPermissions(perm uint8) {
+	r.Permissions = 0
+}
+
+func (r *Role) HasPermission(perm uint8) bool {
+	return (r.Permissions & perm) == perm
+}
+
+func InsertRoles() {
+	roles := make(map[string][]uint8)
+
+	roles["User"] = []uint8{USER}
+	roles["Event-Organizer"] = []uint8{USER, CREATE_EVENT, MODIFY_EVENT, DELETE_EVENT}
+	roles["Moderator"] = []uint8{USER, CREATE_EVENT, MODIFY_EVENT, DELETE_EVENT, MODERATE}
+	roles["Admin"] = []uint8{USER, CREATE_EVENT, MODIFY_EVENT, DELETE_EVENT, MODERATE, ADMIN}
+
+	default_role := "User"
+	// for r := range roles {
+	// 	role := db
+	// }
+
+	fmt.Println(roles, default_role)
 }
